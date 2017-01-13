f4a463251d406ee977b41c7eb4fd92497b85bca8
Mark Oates <euph_boy@hotmail.com>
2017-01-12 23:15:11 -0500

    Allow users to signup (add UserController create action)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :users, param: :slug, only: [:show]
+  resources :users, param: :slug, only: [:show, :create]
   get '/signup', to: 'users#new'
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,4 +24,28 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_template :new
   end
+
+  test 'POST #create redirects to the user page successfuly' do
+    valid_new_user_params = {
+      username: 'Joe Valid',
+      email: 'valid@email.com',
+      password: 'valid1pass',
+      password_confirmation: 'valid1pass'
+    }
+
+    post users_path, params: { user: valid_new_user_params }
+    assert_redirected_to '/users/joe-valid'
+  end
+
+  test 'POST #create redirects to signup when invalid user params are passed' do
+    invalid_new_user_params = {
+      username: 'Joe InValid',
+      email: 'invalidemail',
+      password: 'pass',
+      password_confirmation: 'pass2'
+    }
+
+    post users_path, params: { user: invalid_new_user_params }
+    assert_redirected_to '/signup'
+  end
 end
