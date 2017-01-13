d52a71783b9a9546e36182c149c01cb9b6783d2d
Mark Oates <euph_boy@hotmail.com>
2017-01-12 20:20:34 -0500

    Raise RecordNotFound if when a User does not exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(slug: params[:slug])
+    @user = User.find_by(slug: params[:slug]) or record_not_found
   end
 end
