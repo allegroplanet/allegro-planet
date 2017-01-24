2c5ac4134ed835551948c7c621fef80138da177f
Mark Oates <euph_boy@hotmail.com>
2017-01-23 20:35:09 -0500

    Add RailsAdmin config for User

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,4 +8,32 @@ RailsAdmin.config do |config|
     delete
     show_in_app
   end
+
+  config.model 'User' do
+    object_label_method do
+      :username
+    end
+
+    list do
+      exclude_fields :created_at,
+        :email,
+        :updated_at,
+        :password_digest,
+        :slug,
+        :website,
+        :description
+    end
+
+    show do
+      exclude_fields :password_digest
+    end
+
+    edit do
+      exclude_fields :created_at,
+        :password_digest
+      configure :slug do
+        read_only true
+      end
+    end
+  end
 end
