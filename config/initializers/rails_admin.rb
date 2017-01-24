8ac89ca372f4c2618f1f27e911e6322e9123e2a2
Mark Oates <euph_boy@hotmail.com>
2017-01-23 19:37:59 -0500

    Mount RailsAdmin route; Add RailsAdmin config initializer

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,11 @@
+RailsAdmin.config do |config|
+  config.actions do
+    dashboard
+    index
+    new
+    show
+    edit
+    delete
+    show_in_app
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'welcome#index'
 
   get '/depot', to: 'games#index'
