bb09ab6649beb3172f0443d608cfd0355704c93d
Mark Oates <euph_boy@hotmail.com>
2017-01-29 19:38:09 -0500

    Add tests for Game properties

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,10 @@
 class Game < ApplicationRecord
+  validates :title,
+    presence: true,
+    allow_blank: false,
+    string_format: { rules: [:starts_with_non_whitespace, :ends_with_non_whitespace, :has_only_printable_characters] }
+  validates :description,
+    presence: true,
+    allow_blank: false,
+    string_format: { rules: [:starts_with_non_whitespace, :ends_with_non_whitespace, :has_only_printable_characters] }
 end
