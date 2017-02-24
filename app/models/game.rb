cda8393d3fa18a609040134f7dd3e1941dd51afe
Mark Oates <euph_boy@hotmail.com>
2017-02-23 20:56:23 -0500

    Add cosmetic blank space in Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,7 @@ class Game < ApplicationRecord
     presence: true,
     allow_blank: false,
     string_format: { rules: [:starts_with_non_whitespace, :ends_with_non_whitespace, :has_only_printable_characters] }
+
   validates :description,
     presence: true,
     allow_blank: false,
