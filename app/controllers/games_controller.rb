60fc5382889c87c46269e86a783847f990455788
Mark Oates <euph_boy@hotmail.com>
2017-02-24 00:29:53 -0500

    Add rudimentary GamesController show action w/view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,8 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
   def show
     @game = Game.find(params[:id])
     @screenshot_urls = @game.game_screenshots.collect { |s| s.image.url }
