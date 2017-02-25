b55854f707df1d1c496b1a8e67f06485007c8c78
Mark Oates <euph_boy@hotmail.com>
2017-02-25 13:15:22 -0500

    Add RailsAdmin config for Game

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -9,6 +9,8 @@ RailsAdmin.config do |config|
     show_in_app
   end
 
+  # User
+
   config.model 'User' do
     object_label_method do
       :username
@@ -31,6 +33,18 @@ RailsAdmin.config do |config|
     edit do
       exclude_fields :created_at,
         :password_digest
+      configure :slug do
+        read_only true
+      end
+    end
+  end
+
+  # Game
+
+  config.model 'Game' do
+    edit do
+      exclude_fields :created_at,
+        :updated_at
       configure :slug do
         read_only true
       end
