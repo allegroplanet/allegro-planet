2745e1f85653e5fc54024c784f19439e018b4914
Mark Oates <euph_boy@hotmail.com>
2017-02-25 12:29:22 -0500

    Update Games endpoint to use slug

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/depot', to: 'games#index'
-  get '/depot/games/:id', to: 'games#show'
+  get '/depot/games/:slug', to: 'games#show'
 
   resources :users, param: :slug, only: [:show]
   get '/signup', to: 'users#new'
