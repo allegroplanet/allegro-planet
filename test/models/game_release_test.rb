a90d20a556ebc264fe3bc6571444fa49a34842b5
Mark Oates <euph_boy@hotmail.com>
2017-02-26 02:50:14 -0500

    Add validations and tests for GameRelease

--- test/models/game_release_test.rb
+++ test/models/game_release_test.rb
@@ -5,8 +5,52 @@ class GameReleaseTest < ActiveSupport::TestCase
     GameRelease.reflect_on_all_associations(association_type).select { |a| a.name == property }
   end
 
+  def game_release
+    @game_release ||= GameRelease.new({
+      version_num: 'v0.2.1',
+      notes: 'Added faster clock',
+      game: games(:alex_adventures)
+    })
+  end
+
+  test 'validates without errors' do
+    game_release.validate
+    assert game_release.errors.empty?
+  end
+
+  test 'without a version_num, is invalid' do
+    game_release.version_num = nil
+    game_release.validate
+    assert_includes game_release.errors[:version_num], "can't be blank"
+  end
+
+  test 'version_num must be unique for that game' do
+    game_release.game = games(:alex_adventures)
+    game_release.version_num = game_release.game.game_releases.first.version_num
+    game_release.validate
+    assert_includes game_release.errors[:version_num], 'already exists'
+  end
+
+  test 'version_num does not need to be unique globally' do
+    game_release.game = games(:alex_adventures)
+    game_release.version_num = games(:super_biker).game_releases.first.version_num
+    game_release.validate
+    refute_includes game_release.errors[:version_num], 'already exists'
+  end
+
+  test 'version_num is formatted with the expected rules' do
+    expected_validation_rules = [:starts_with_non_whitespace, :ends_with_non_whitespace, :has_only_printable_characters]
+    assert_validates_format_rules expected_validation_rules, GameRelease, :version_num
+  end
+
   test 'belongs to a game' do
     associations = game_release_associations(:belongs_to, :game)
     assert associations.one?
+  end
+
+  test 'without a game, is invalid' do
+    game_release.game = nil
+    game_release.validate
+    assert_includes game_release.errors[:game], 'must exist'
   end
 end
