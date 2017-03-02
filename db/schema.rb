66c4e674bf383eb56aa58faf958d8aa4009e57d0
Mark Oates <euph_boy@hotmail.com>
2017-03-02 01:47:27 -0500

    Update schema after adding index on handles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302063921) do
+ActiveRecord::Schema.define(version: 20170302064617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170302063921) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "handle"
+    t.index ["handle"], name: "index_games_on_handle", using: :btree
   end
 
   create_table "games_users", id: false, force: :cascade do |t|
@@ -67,6 +68,7 @@ ActiveRecord::Schema.define(version: 20170302063921) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "handle"
+    t.index ["handle"], name: "index_users_on_handle", using: :btree
   end
 
   add_foreign_key "game_releases", "games"
