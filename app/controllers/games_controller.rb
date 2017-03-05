818a60f97e4e2277726bb0458110ea0328149724
Mark Oates <euph_boy@hotmail.com>
2017-03-04 19:41:09 -0500

    Format Game description as markdown in #show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,6 +5,7 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find_by(handle: params[:handle]) or record_not_found
+    @description = MarkdownConverter.new(@game.description).html.html_safe
     @screenshot_urls = @game.game_screenshots.collect { |s| s.image.url }
     @releases = @game.game_releases
   end
