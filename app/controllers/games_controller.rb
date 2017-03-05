1bd939c7c31a8ef3eaa007b6511676917d73c3d2
Mark Oates <euph_boy@hotmail.com>
2017-03-04 22:51:15 -0500

    (p2) Use GameReleaseDecorator in GameController show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,6 @@ class GamesController < ApplicationController
     @game = Game.find_by(handle: params[:handle]) or record_not_found
     @description = MarkdownConverter.new(@game.description).html
     @screenshot_urls = @game.game_screenshots.collect { |s| s.image.url }
-    @releases = @game.game_releases
+    @releases = @game.game_releases.collect { |r| GameReleaseDecorator.new(r) }
   end
 end
