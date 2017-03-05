b4008312a7dfd8dc14a57250ec8a9db906fb4864
Mark Oates <euph_boy@hotmail.com>
2017-03-05 00:49:09 -0500

    Add #public_url to GameScreenshot model

--- app/models/game_screenshot.rb
+++ app/models/game_screenshot.rb
@@ -2,4 +2,8 @@ class GameScreenshot < ApplicationRecord
   mount_uploader :image, GameScreenshotUploader
 
   belongs_to :game
+
+  def public_url
+    "https://#{Figaro.env.aws_allegro_planet_bucket}.s3.amazonaws.com/#{image.path}"
+  end
 end
