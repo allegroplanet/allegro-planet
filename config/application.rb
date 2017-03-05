6a92734fec732eb67ffd7c34fc858ff1ea6d3717
Mark Oates <euph_boy@hotmail.com>
2017-03-04 21:05:01 -0500

    Move MarkdownConverter to /app/converters

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module AllegroPlanet
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += [
+      "#{config.root}/app/converters",
+    ]
   end
 end
