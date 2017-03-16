23e07082a385f3e318cae71007ff2afa79584569
Mark Oates <euph_boy@hotmail.com>
2017-03-15 22:23:15 -0400

    Add 'feature', 'tile', and 'thumb' versions of GameScreenshots

--- app/uploaders/game_screenshot_uploader.rb
+++ app/uploaders/game_screenshot_uploader.rb
@@ -1,6 +1,24 @@
 class GameScreenshotUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  VERSION_FEATURE_SIZE = [640, 360] # 1/3 HD
+  VERSION_TILE_SIZE    = [320, 180] # 1/6 HD
+  VERSION_THUMB_SIZE   = [160, 90]  # 1/12 HD
+
   def store_dir
     'screenshots'
+  end
+
+  version :feature do
+    process resize_to_fit: VERSION_FEATURE_SIZE
+  end
+
+  version :tile do
+    process resize_and_pad: VERSION_TILE_SIZE
+  end
+
+  version :thumb do
+    process resize_and_pad: VERSION_THUMB_SIZE
   end
 
   def extension_whitelist
