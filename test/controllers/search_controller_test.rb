f0e231a81adae3a1353a4f01f6e34ea2504157aa
Mark Oates <euph_boy@hotmail.com>
2017-09-02 16:58:35 -0400

    Add SearchController test for #index with no params

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
   test 'GET #index is successful' do
+    get search_url
+    assert_response :success
+  end
+
+  test 'GET #index with a query is successful' do
     get search_url, params: { q: 'duck' }
     assert_response :success
   end
