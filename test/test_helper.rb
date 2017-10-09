37d3adc4a1599cbdf0d283d29b34e0cec3d251f4
Mark Oates <euph_boy@hotmail.com>
2017-10-09 01:45:19 -0400

    Use assert_association in GameTest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'string_format_assertions'
+require 'association_assertions'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
