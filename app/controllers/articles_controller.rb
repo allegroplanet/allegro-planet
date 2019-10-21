9a58750f580fa8978cd9fe37f5ae40f9bfeb3726
Mark Oates <markoates0@gmail.com>
2019-10-20 22:25:44 -0400

    Add tests in "ArticlesControllerTest"; Fix "published" access of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.published
   end
 
   def show
-    article = Article.find_by(handle: article_handle_param)
+    article = Article.published.find_by(handle: article_handle_param) or record_not_found
 
     @title = article.title
     @body_html = MarkdownConverter.new(article.body_markdown).html
